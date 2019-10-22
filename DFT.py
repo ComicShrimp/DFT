4795bc300202a2ebac9fc9dd4960d4968b555d1c
Mário <mario@pop-os.localdomain>
2019-10-22 20:38:26 -0300

    Adiciona Grafico ao calculo

--- DFT.py
+++ DFT.py
@@ -2,21 +2,18 @@ import math
 
 
 def funcaoXn(m):
-    if m == 0:
-        return 1
-    elif m == 1:
-        return 2
-    elif m == 2:
-        return -1
-    elif m == 3:
-        return 1
-    else:
-        return 0
+    return 9
 
+
+usarGraph = False
+if input('Usar Gráfico(Requer Maplotlib): ') == 's':
+    import matplotlib.pyplot as plt
+    usarGraph = True
 
 quantidadeDePontos = int(input('Digite a Quantidade de Pontos: '))
 
-resultado = 0
+senoGrafico = []
+cossenoGrafico = []
 for m in range(0, quantidadeDePontos):
     cosseno = seno = 0
 
@@ -27,7 +24,16 @@ for m in range(0, quantidadeDePontos):
         seno += math.sin((2 * math.pi * m * n) /
                          quantidadeDePontos) * funcaoXn(n)
 
+    cossenoGrafico.append(cosseno)
+    senoGrafico.append(seno)
+
     # Necessário por conta do sinal na equação
     seno = seno * -1
     print('X[{}] = {:^6.2f} {:>2} {:>6.2f}j'.format(
         m, cosseno, '+' if seno >= 0 else '-', seno if seno > 0 else seno * -1))
+
+if usarGraph:
+  plt.plot(cossenoGrafico)
+  plt.title('Parte Real')
+  plt.grid(True)
+  plt.show()
