c0d4bab3c1b15720081b8e98cb20ba3740539589
Mário <mario@pop-os.localdomain>
2019-10-23 11:35:34 -0300

    Melhora o Grafico
    Agora mostra a parte imaginária no grafico e melhora a vizualização

--- DFT.py
+++ DFT.py
@@ -2,7 +2,8 @@ import math
 
 
 def funcaoXn(m):
-    return 9
+    resultado = [0, 1, 0, 0]
+    return resultado[m]
 
 
 usarGraph = False
@@ -33,7 +34,16 @@ for m in range(0, quantidadeDePontos):
         m, cosseno, '+' if seno >= 0 else '-', seno if seno > 0 else seno * -1))
 
 if usarGraph:
-  plt.plot(cossenoGrafico)
+  # Grafico Parte Real
+  plt.subplot(1, 2, 1)
   plt.title('Parte Real')
   plt.grid(True)
+  plt.plot(cossenoGrafico, 'o--')
+
+  # Gráfico Parte Imaginaria
+  plt.subplot(1, 2, 2)
+  plt.title('Parte Imaginária')
+  plt.grid(True)
+  plt.plot(senoGrafico, color='tab:orange', linestyle = '--', marker='o')
+
   plt.show()
